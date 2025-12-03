7ecd20bc86cab7d066672b8283be0b85f13e5513
Sahil Chouhan <chouhangolu604@gmail.com>
2025-12-03 11:45:18 +0530

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import UseActionHook from './components/UseActionHook'
 import UseIDHook from './components/UseIDHook'
+import Fragments from './components/Fragments'
 
 const App = () => {
   return (
     <>
      {/* <UseActionHook />  */}
-     <UseIDHook />
+     {/* <UseIDHook /> */}
+     <Fragments />
     </>
   )
 }
---
+++ src/components/Fragments.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Fragments = () => {
+  return (
+    <div>
+      <h1>Fragmens in React</h1>
+      <p>It can remove extra div element</p>
+    </div>
+  )
+}
+
+export default Fragments
