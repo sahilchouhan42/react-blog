2ca913bac01ff5a018254d1f23637afeda16edca
Sahil Chouhan <chouhangolu604@gmail.com>
2025-12-06 11:41:23 +0530

    new custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,43 @@ import UseIDHook from './components/UseIDHook'
 import Fragments from './components/Fragments'
 import College2 from './component2/College2'
 import { SubjectContext } from './component2/ContextData'
+import useToggle from './Hooks/useToggle'
 
 const App = () => {
-  const [subject,setSubject] = useState("English")
+  const [value, toggleValue] = useToggle(true)
+  const [data, setData] = useToggle(true)
+  // const [subject,setSubject] = useState("English")
   return (
-    <div style={{backgroundColor: 'yellow', padding: "10px"}}>
-      <SubjectContext.Provider value={subject}>
+    <div>
+    {/* <div style={{backgroundColor: 'yellow', padding: "10px"}}> */}
+      {/* <SubjectContext.Provider value={subject}>
         <select value={subject} onChange={(event)=>setSubject(event.target.value)}>
           <option value="">Select Subject</option>
           <option value="Maths">Maths</option>
           <option value="History">History</option>
           <option value="Hindi">Hindi</option>
-        </select>
-      <h1>Context API</h1>
+        </select> */}
+      {
+        value?<h1>Custom Hooks in React JS</h1>:null
+      }
+      <button onClick={toggleValue}>Toggle Heading</button>
+      <button onClick={()=>toggleValue(false)}>Hide Heading</button>
+      <button onClick={()=>toggleValue(true)}>Show Heading</button>
+      <hr /><hr />
+      {
+        data?<h1>Second Heading</h1>:null
+      }
+      <button onClick={setData}>Toggle Second Heading</button>
+      <button onClick={()=>setData(false)}>Hide Second Heading</button>
+      <button onClick={()=>setData(true)}>Show Second Heading</button>
       
      {/* <UseActionHook />  */}
      {/* <UseIDHook /> */}
      {/* <Fragments /> */}
-     <College2 />
+     {/* <College2 />
      <button onClick={()=>setSubject('')}>Clear Subject</button>
-     </SubjectContext.Provider>
+     </SubjectContext.Provider> */}
+    {/* </div> */}
     </div>
   )
 }
